4182bb64ca0f46081f5cf3f3d8269a95506b8f13
Nivedita Ravishankar <143220750+nravish1@users.noreply.github.com>
2024-02-24 21:00:52 -0500

    Add files via upload

---
+++ damnit2.py
@@ -0,0 +1,20 @@
+from distutils.log import debug 
+from fileinput import filename 
+from flask import *  
+app = Flask(__name__)   
+  
+@app.route('/')   
+def main():   
+    return render_template("idk.html")   
+
+
+@app.route('/success', methods = ['POST'])   
+def success():   
+    if request.method == 'POST':   
+        f = request.files['file'] 
+        f.save('documents/'+f.filename)   
+        return render_template("totesdidit.html", name = f.filename)   
+  
+if __name__ == '__main__':   
+    app.run(debug=True)
+
